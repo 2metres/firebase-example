ad5e2fd86ddc8c8dee6be712acc11f33cc86fec7
Andrew Carr <acarr@raywhite.com>
2017-11-21 16:39:19 +1100

    Pull in gatsby starter

---
+++ src/layouts/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+
+import './index.css'
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="Gatsby Default Starter"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <header>
+      <div>
+        <h1>
+          <Link to="/">Gatsby</Link>
+        </h1>
+      </div>
+    </header>
+    <main>
+      { children() }
+    </main>
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
