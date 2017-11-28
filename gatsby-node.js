2a62f03dac1dc306f2607f58f9e5c2e10418284b
Andrew Carr <andrew@2metres.com>
2017-11-28 12:33:33 +1100

    Update regex

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,8 @@
-const path = require('path');
+const path = require('path')
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators;
-
-  return new Promise((resolve) => {
+  const { createPage } = boundActionCreators
+  return new Promise((resolve, reject) => {
     graphql(`
       {
         allPrismicDocument {
@@ -17,23 +16,33 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
     `).then(result => {
       result.data.allPrismicDocument.edges.map(({ node }) => {
         createPage({
-          path: `/posts/${ node.slugs[0] }`,
-          component: path.resolve('./src/templates/post.js'),
+          path: `/posts/${node.slugs[0]}`,
+          component: path.resolve(`./src/templates/post.js`),
           context: {
             slug: node.slugs[0],
           },
-        });
-      });
-      resolve();
-    });
-  });
-};
+        })
+      })
+      resolve()
+    })
+  })
+}
 
 exports.modifyWebpackConfig = ({ config, stage }) => {
   switch (stage) {
     case 'develop': {
       config.loader('sassModules', (current) => {
-        current.loaders[1] = current.loaders[1].replace(/localIdentName=.*?(&|$)/, 'localIdentName=[folder]__[local]&');
+        current.loaders[1] = current.loaders[1].replace(/localIdentName=.*?(&|!|$)/, 'localIdentName=[folder]__[local]&');
+        return current;
+      });
+      return config;
+    }
+    case 'develop-html':
+    case 'build-css':
+    case 'build-html':
+    case 'build-javascript': {
+      config.loader('sassModules', (current) => {
+        current.loader = current.loader.replace(/localIdentName=.*?(&|!|$)/, 'localIdentName=[folder]__[local]---[hash:base64:5]!');
         return current;
       });
       return config;
