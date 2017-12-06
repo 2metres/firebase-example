4f19a3142401f09ea5a243817a0535aba913e6e5
Andrew Carr <andrew@2metres.com>
2017-12-07 10:51:35 +1100

    Webpack config and liquid templates to get started

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const Prismic = require('prismic-javascript');
+const prismicHtml = require('prismic-dom').RichText.asHtml;
+const prismicText = require('prismic-dom').RichText.asText;
+
+require('dotenv').config()
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './public'),
+  },
+  module: {
+    rules: [{
+      test: /\.html$/,
+      use: [
+        {
+          loader: "html-loader"
+        },
+        {
+          loader: "liquid-loader",
+          options: {
+            data: {
+              homepage: Prismic.api(process.env.PRISMIC_API_URL)
+                .then(api => api.query('[ at(document.type, "homepage") ]'))
+                .then(response => response.results[0])
+                .then(res => ({
+                    id: res.id,
+                    title: prismicText(res.data.homepage.title.value),
+                    description: prismicText(res.data.homepage.description.value),
+                    contents: prismicHtml(res.data.homepage.contents.value),
+                  })
+                ),
+
+              posts: Prismic.api(process.env.PRISMIC_API_URL)
+                .then(api => api.query('[ at(document.type, "post") ]'))
+                .then(response => response.results)
+                .then(res => {
+                  return res.map(post => (
+                    {
+                      id: post.id,
+                      slug: post.slugs[0],
+                      title: prismicText(post.data.post.title.value),
+                      contents: prismicHtml(post.data.post.contents.value),
+                    }
+                  ))
+                })
+            }
+          }
+      }]
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ]
+};
