548c50a22e8569e0d4e2bf7898727c1497728678
Andrew Carr <andrew@2metres.com>
2017-12-06 15:43:21 +1100

    Get iterators working

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const cssStandards = require('spike-css-standards')
 const jsStandards = require('spike-js-standards')
 const pageId = require('spike-page-id')
 const Records = require('spike-records')
+
 const Prismic = require('prismic-javascript')
+const PrismicDOM = require('prismic-dom')
 
 require('dotenv').config()
 
@@ -20,26 +22,40 @@ const homepageReq = () => (
 const postsReq = () => (
   Prismic.api(process.env.PRISMIC_API_URL)
     .then(api => api.query('[ at(document.type, "post") ]'))
-    .then(response =>  response.results)
+    .then(response => response.results)
 )
+
 
 module.exports = {
   devtool: 'source-map',
   ignore: [
+    'firebase.json',
     '**/layout.html',
     '**/_*',
     '**/.*',
     'readme.md',
+    'LICENSE',
     'yarn.lock',
+    'package-lock.json',
   ],
   reshape: htmlStandards({
-    locals: () => locals
+    locals: () => locals,
+    content: {
+      'prismic-html': (data) => PrismicDOM.RichText.asHtml(JSON.parse(data)),
+      'prismic-text': (data) => PrismicDOM.RichText.asText(JSON.parse(data))
+    }
   }),
   plugins: [
     new Records({
       addDataTo: locals,
       homepage: { callback: homepageReq },
-      posts: { callback: postsReq },
+      posts: {
+        callback: postsReq,
+        template: {
+          path: 'templates/post.html',
+          output: (post) => `posts/${post.slugs[0]}.html`
+        }
+      },
     })
   ],
   postcss: cssStandards({
